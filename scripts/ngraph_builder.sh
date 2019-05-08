d7e14edfe36df0fbcd7c7cf55a3be5ece54124b5
Pawel Piotrowicz <pablo.piotrowicz@gmail.com>
2019-05-08 11:35:26 +0200

    save

--- scripts/ngraph_builder.sh
+++ scripts/ngraph_builder.sh
@@ -1,32 +1,75 @@
 #!/bin/bash
 ulimit -n 65535
-
+CORES="50"
 is_debug=0
 is_cmake=0
 is_build=0
-is_infer=0
+is_infer_python=0
+is_build_capi_app_infer_image=0
+is_build_capi=0
+is_infer_capi=0
+is_build_paddlepaddle=0
 
 if [ ! -f ref.txt ]; then
    echo "ref.txt not found run pre script"
    exit 1
 fi
 
+#declare -A options
+
+#options["input"]="runner"
+
+#${options["input"]}
+
+
+ROOT_DIR=`pwd`
+log="$ROOT_DIR/commands.txt"
+
+echo > $log
+
+function exe_cmd() {
+
+   echo -e '\033[0;31m'  $1  '\033[0m'
+   echo $1 >> $log
+
+   $1
+
+   if [ $? -ne 0 ]; then
+      echo "FAIL on $1" >> $log
+   fi
+
+}
+
+#exe_cmd "ls -l"
+
+#exit 1
+
 
 source ref.txt
 
 function usage() {
 
-     echo "Usage:"
-     echo "Usage [--debug]  --cmake --build --run-infer" 
+     echo "Usage:  [---show-python-path]"
+     echo "Usage:  [--debug]  --cmake --build --run-infer-python" 
+     echo "Usage:  [--debug] --build-capi --run-infer-capi" 
+     echo "Usage:  [--debug] --build-paddlepaddle --build-capi-app-infer-image"
  
 }
 
 echo $PADDLE_REF
 
-ROOT_DIR=`pwd`
+
+
 MODEL_DIR="$ROOT_DIR/paddle-models"
+MODEL_CAPI_DIR="$MODEL_DIR/fluid/image_classification/capi/"
+#OPT_MODEL_CAPI_DIR_BUILD="$MODEL_CAPI_DIR/build"
+OPT_MODEL_CAPI_DIR_BUILD=""
+
+
+
 build_dir_release="$ROOT_DIR/$PADDLE_REF/build_release"
 build_dir_debug="$ROOT_DIR/$PADDLE_REF/build_debug"
+build_dir_paddlepaddle=""
 
 IIC_SCRIPT_DIR="$ROOT_DIR/paddle-models/fluid/image_classification/"
 IIC_SCRIPT="$IIC_SCRIPT_DIR/infer_image_classification.py"
@@ -40,6 +83,155 @@ SAVE_MODELS_DIR="$ROOT_DIR/save_models_extract_dir"
 OPT_PADDLE_BUILD_DEBUG="$build_dir_debug/python:$build_dir_debug/paddle"
 OPT_PADDLE_BUILD_RELEASE="$build_dir_release/python:$build_dir_release/paddle"
 
+
+##########################
+#   BIG_DATA_SET_CONFIG
+##########################
+
+BIG_DATASET_DIR_DATA="/home/pawepiot/BIG_DATASET/imagenet/"
+BIG_DATASET_LIST_FILE="$BIG_DATASET_DIR_DATA/val_list.txt"
+
+
+
+
+function build_capi_app_infer_image()
+{
+   
+     if [ ! -d "$MODEL_DIR" ]; then
+            echo "ERROR: first you have to download repository $MODEL_DIR"
+            exit 1
+     fi
+
+    
+      if [ ! -d "$MODEL_CAPI_DIR" ]; then
+         exe_cmd "cd $MODEL_DIR"
+         exe_cmd "git checkout develop-integration"
+       fi
+    
+
+
+     if [ ! -d "$OPT_MODEL_CAPI_DIR_BUILD" ]; then
+            exe_cmd "mkdir $OPT_MODEL_CAPI_DIR_BUILD"
+     fi
+    
+    
+     exe_cmd "cd $OPT_MODEL_CAPI_DIR_BUILD"
+ 
+    OPT_PADDLE_BUILD=$build_dir_release
+    TYPE_COMPILE="Release"
+
+    if [ $is_debug -eq 1 ]; then
+       TYPE_COMPILE="Debug"
+       OPT_PADDLE_BUILD=$build_dir_debug
+   
+    fi
+
+      
+    if [ ! -d "$OPT_PADDLE_BUILD/fluid_install_dir/" ]; then
+       echo "Error: paddle build does not exist ... fluid_install_dir" 
+       exit 1
+    fi
+
+    exe_cmd "cmake .. -DCMAKE_BUILD_TYPE=$TYPE_COMPILE -DPADDLE_ROOT=$OPT_PADDLE_BUILD/fluid_install_dir/"
+    exe_cmd "make -j $CORES"
+
+
+
+}
+
+
+
+function build_capi()
+{
+
+     if [ ! -d "$MODEL_DIR" ]; then
+            echo "ERROR: first you have to download repository $MODEL_DIR"
+            exit 1
+     fi
+
+     if [ ! -d "$MODEL_CAPI_DIR" ]; then
+         exe_cmd "cd $MODEL_DIR"
+         exe_cmd "git checkout develop-integration"
+     fi
+    
+
+     if [ ! -d "$OPT_MODEL_CAPI_DIR_BUILD" ]; then
+            exe_cmd "mkdir $OPT_MODEL_CAPI_DIR_BUILD"
+     fi
+    
+    
+    exe_cmd "cd $OPT_MODEL_CAPI_DIR_BUILD"
+    
+
+    OPT_PADDLE_BUILD=$build_dir_release
+    TYPE_COMPILE="Release"
+
+    if [ $is_debug -eq 1 ]; then
+       TYPE_COMPILE="Debug"
+       OPT_PADDLE_BUILD=$build_dir_debug
+   
+    fi
+
+  
+
+    exe_cmd "cmake .. -DCMAKE_BUILD_TYPE=$TYPE_COMPILE -DPADDLE_ROOT=$OPT_PADDLE_BUILD/fluid_install_dir/"
+   
+   # cmake .. -DCMAKE_BUILD_TYPE=$TYPE_COMPILE -DPADDLE_ROOT=$OPT_PADDLE_BUILD/fluid_install_dir/
+
+    exe_cmd "make -j $CORES"    
+   # make -j $CORES
+
+}
+
+
+
+function run_infer_capi() 
+{
+
+
+    if [ ! -f "$BIG_DATASET_LIST_FILE" ]; then
+        echo "[ERROR]: DataList file does not exist $BIG_DATASET_LIST_FILE"
+        exit
+    fi
+
+     exe_cmd "cd $OPT_MODEL_CAPI_DIR_BUILD"
+                                                                                       
+     exe_cmd "numactl --membind=0 --physcpubind=0-19 ./infer_image_classification --infer_model="$SAVE_MODELS_DIR/$USE_MODEL" --data_list=$BIG_DATASET_LIST_FILE --data_dir=$BIG_DATASET_DIR_DATA --skip_batch_num=0 --batch_size=32 --iterations=100 --profile --paddle_num_threads=20 --use_mkldnn"
+
+     cd $ROOT_DIR
+
+
+}
+
+
+function build_paddlepaddle()
+{
+
+        if [ ! -d $build_dir_paddlepaddle ]; then
+           echo "Create directory $build_dir_paddlepaddle"
+           exe_cmd "mkdir $build_dir_paddlepaddle" 
+        fi
+        
+        exe_cmd "cd $build_dir_paddlepaddle"
+
+
+     TYPE_COMPILE="Release"
+
+    if [ $is_debug -eq 1 ]; then
+       TYPE_COMPILE="Debug"
+    fi
+
+
+    exe_cmd "cmake .. -DCMAKE_BUILD_TYPE=$TYPE_COMPILE -DWITH_DOC=OFF -DWITH_GPU=OFF -DWITH_DISTRIBUTE=OFF -DWITH_MKLDNN=ON -DWITH_MKL=ON -DWITH_GOLANG=OFF -DWITH_STYLE_CHECK=OFF -DWITH_TESTING=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DWITH_PROFILER=OFF -DWITH_NGRAPH=ON -DWITH_FLUID_ONLY=ON"
+
+    exe_cmd "make -j $CORES"
+    exe_cmd "make -j $CORES  inference_lib_dist"
+
+
+
+}
+
+
 function build_release()
 {
 
@@ -48,14 +240,15 @@ function build_release()
    
     if [ ! -d $build_dir_release ]; then
          echo "Create directory $build_dir_release"
-         mkdir $build_dir_release 
+         exe_cmd "mkdir $build_dir_release" 
     fi
 
-    cd $build_dir_release
+    exe_cmd "cd $build_dir_release"
 
-    cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_DOC=OFF -DWITH_GPU=OFF -DWITH_DISTRIBUTE=OFF -DWITH_MKLDNN=ON -DWITH_MKL=ON -DWITH_GOLANG=OFF -DWITH_STYLE_CHECK=OFF -DWITH_TESTING=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DWITH_PROFILER=OFF -DWITH_NGRAPH=ON
+    exe_cmd "cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_DOC=OFF -DWITH_GPU=OFF -DWITH_DISTRIBUTE=OFF -DWITH_MKLDNN=ON -DWITH_MKL=ON -DWITH_GOLANG=OFF -DWITH_STYLE_CHECK=OFF -DWITH_TESTING=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DWITH_PROFILER=OFF -DWITH_NGRAPH=ON -DWITH_FLUID_ONLY=ON"
 
-    make -j50
+    exe_cmd "make -j $CORES"
+    exe_cmd "make -j $CORES  inference_lib_dist"
 
 
 }
@@ -69,12 +262,12 @@ function build_release_cmake_new() {
 
     if [ ! -d $build_dir_release ]; then
          echo "Create directory $build_dir_release"
-         mkdir $build_dir_release
+         exe_cmd "mkdir $build_dir_release"
     fi
 
-    cd $build_dir_release
+    exe_cmd "cd $build_dir_release"
 
-    cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_GPU=OFF -DWITH_MKLDNN=ON -DWITH_TESTING=ON -DWITH_PROFILER=ON -DWITH_STYLE_CHECK=OFF -DON_INFER=ON -DWITH_NGRAPH=ON
+    exe_cmd "cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_FLUID_ONLY=ON -DWITH_GPU=OFF -DWITH_MKLDNN=ON -DWITH_TESTING=ON -DWITH_PROFILER=ON -DWITH_STYLE_CHECK=OFF -DON_INFER=ON -DWITH_NGRAPH=ON"
 
    # make -j50
 
@@ -92,14 +285,15 @@ function build_release_new() {
 
     if [ ! -d $build_dir_release ]; then
          echo "Create directory $build_dir_release"
-         mkdir $build_dir_release
+         exe_cmd "mkdir $build_dir_release"
     fi
 
-    cd $build_dir_release
+    exe_cmd "cd $build_dir_release"
 
   #  cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_GPU=OFF -DWITH_MKLDNN=ON -DWITH_TESTING=ON -DWITH_PROFILER=ON -DWITH_STYLE_CHECK=OFF -DON_INFER=ON -DWITH_NGRAPH=ON
 
-    make -j50
+    exe_cmd "make -j $CORES"
+    exe_cmd "make -j $CORES  inference_lib_dist"
 
 
 
@@ -114,16 +308,13 @@ function build_debug_cmake()
 
     if [ ! -d $build_dir_debug ]; then
          echo "Create directory $build_dir_debug"
-         mkdir $build_dir_debug
+         exe_cmd "mkdir $build_dir_debug"
     fi
 
-    cd $build_dir_debug
 
-    cmake .. -DCMAKE_BUILD_TYPE=Debug -DWITH_DOC=OFF -DWITH_GPU=OFF -DWITH_DISTRIBUTE=OFF -DWITH_MKLDNN=ON -DWITH_MKL=ON -DWITH_GOLANG=OFF -DWITH_STYLE_CHECK=OFF -DWITH_TESTING=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DWITH_PROFILER=OFF -DWITH_NGRAPH=ON
+    exe_cmd "cd $build_dir_debug"
 
-   # make -j50
-
-
+    exe_cmd "cmake .. -DCMAKE_BUILD_TYPE=Debug -DWITH_FLUID_ONLY=ON -DWITH_DOC=OFF -DWITH_GPU=OFF -DWITH_DISTRIBUTE=OFF -DWITH_MKLDNN=ON -DWITH_MKL=ON -DWITH_GOLANG=OFF -DWITH_STYLE_CHECK=OFF -DWITH_TESTING=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DWITH_PROFILER=OFF -DWITH_NGRAPH=ON"
 
 }
 
@@ -137,16 +328,13 @@ function build_debug()
 
     if [ ! -d $build_dir_debug ]; then
          echo "Create directory $build_dir_debug"
-         mkdir $build_dir_debug
+        exe_cmd "mkdir $build_dir_debug"
     fi
 
-    cd $build_dir_debug
+    exe_cmd "cd $build_dir_debug"
 
-  #  cmake .. -DCMAKE_BUILD_TYPE=Debug -DWITH_DOC=OFF -DWITH_GPU=OFF -DWITH_DISTRIBUTE=OFF -DWITH_MKLDNN=ON -DWITH_MKL=ON -DWITH_GOLANG=OFF -DWITH_STYLE_CHECK=OFF -DWITH_TESTING=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DWITH_PROFILER=OFF -DWITH_NGRAPH=ON
-
-    make -j50
-
-
+    exe_cmd "make -j $CORES" 
+    exe_cmd "make -j $CORES inference_lib_dist"
 
 }
 
@@ -156,7 +344,7 @@ function prepare_model()
 {
 
    if [ ! -d "$SAVE_MODELS_DIR" ]; then
-              mkdir $SAVE_MODELS_DIR
+             exe_cmd "mkdir $SAVE_MODELS_DIR"
      fi
 
      if [ ! -d "$SAVE_MODELS_DIR/$USE_MODEL" ]; then
@@ -167,9 +355,33 @@ function prepare_model()
                exit
           fi
 
-          tar -zxvf $MODEL_FILE -C $SAVE_MODELS_DIR
+          exe_cmd "tar -zxvf $MODEL_FILE -C $SAVE_MODELS_DIR"
 
      fi
+
+
+}
+
+
+function showpythonpath() {
+
+
+    echo "**** RUN INFER IMAGE *****"
+
+    if [ ! -f $IIC_SCRIPT ]; then
+        echo "FILE $IIC_SCIRPT not found"
+       exit 1
+    fi
+
+    prepare_model
+     
+    export PYTHONPATH="$OPT_PADDLE_BUILD_DEBUG"
+
+    echo "PYTHONPATH=$PYTHONPATH"
+
+    #cd $IIC_SCRIPT_DIR 
+  #  python infer_image_classification.py --batch_size=1 --data_set=imagenet --iterations=400 --device=CPU --skip_batch_num=10 --infer_model_path=/data/PaddlePaddle/models/paddlepaddle/resnet_50_v1 --profile --use_fake_data
+   # python infer_image_classification.py --batch_size=1 --data_set=imagenet --iterations=400 --device=CPU --skip_batch_num=10 --infer_model_path="$SAVE_MODELS_DIR/$USE_MODEL" --profile --use_fake_data
 
 
 }
@@ -194,8 +406,13 @@ function infer_image_debug() {
 
     cd $IIC_SCRIPT_DIR 
   #  python infer_image_classification.py --batch_size=1 --data_set=imagenet --iterations=400 --device=CPU --skip_batch_num=10 --infer_model_path=/data/PaddlePaddle/models/paddlepaddle/resnet_50_v1 --profile --use_fake_data
-    python infer_image_classification.py --batch_size=1 --data_set=imagenet --iterations=400 --device=CPU --skip_batch_num=10 --infer_model_path="$SAVE_MODELS_DIR/$USE_MODEL" --profile --use_fake_data
+ export FLAGS_use_ngraph=true
+ export FLAGS_use_ngraph_cache=true
+ 
+ ccmd="python infer_image_classification.py --batch_size=1 --data_set=imagenet --iterations=400 --device=CPU --skip_batch_num=10 --infer_model_path="$SAVE_MODELS_DIR/$USE_MODEL" --profile --use_fake_data"
+ $ccmd
 
+ echo $ccmd 
 
 }
 
@@ -233,6 +450,16 @@ if [ $# -eq 0 ]; then
 for item in "$@"
 do
     was_used=0
+
+      
+   if [  "$item" = "--show-python-path" ]; then
+           echo ""
+           showpythonpath           
+           exit 0
+   fi
+
+
+
    if [  "$item" = "--debug" ]; then
            echo "--debug found"
            is_debug=1
@@ -251,11 +478,39 @@ do
            was_used=1
    fi
 
-   if [  "$item" = "--run-infer" ]; then
-           echo "--run-infer found"
-           is_infer=1 
+   if [  "$item" = "--run-infer-python" ]; then
+           echo "----run-infer-python found"
+           is_infer_python=1 
            was_used=1
    fi
+
+
+   if [  "$item" = "--run-infer-capi" ]; then
+           echo "----run-infer-capi found"
+           is_infer_capi=1 
+           was_used=1
+   fi
+
+
+   if [  "$item" = "--build-capi" ]; then
+           echo "--build-capi found"
+           is_build_capi=1 
+           was_used=1
+   fi
+
+   if [ "$item" = "--build-capi-app-infer-image" ]; then
+         is_build_capi_app_infer_image=1
+         was_used=1
+   fi
+
+
+
+   if [ "$item" = "--build-paddlepaddle" ]; then
+         is_build_paddlepaddle=1
+         was_used=1
+   fi
+
+
 
    if [ $was_used -eq 0 ]; then
        "ERROR Args is unknown $item" 
@@ -270,16 +525,28 @@ done
 #is_debug=0
 #is_cmake=0
 #is_build=0
-#is_infer=0
+#is_infer_python=0
+
+if [ $is_debug -eq 1 ]; then
+   OPT_MODEL_CAPI_DIR_BUILD="$MODEL_CAPI_DIR/build_debug"
+   build_dir_paddlepaddle=$build_dir_debug
+else
+   OPT_MODEL_CAPI_DIR_BUILD="$MODEL_CAPI_DIR/build_release"
+   build_dir_paddlepaddle=$build_dir_release
+fi
 
 
 if [ $is_cmake -eq 1 ]; then
 
     if [ $is_debug -eq 1 ]; then
         echo "DEBUG IS SET !!"
+
+        
         build_debug_cmake
     else
+        
         build_release_cmake_new
+
     fi
 
 fi
@@ -297,7 +564,7 @@ if [ $is_build -eq 1 ]; then
 fi
 
 
-if [ $is_infer -eq 1 ]; then
+if [ $is_infer_python -eq 1 ]; then
 
      if [ $is_debug -eq 1 ]; then
          echo "DEBUG IS SET !!"
@@ -309,7 +576,28 @@ if [ $is_infer -eq 1 ]; then
 fi
 
 
+if [ $is_build_capi -eq 1 ]; then
 
+      build_capi
+
+fi
+
+
+
+
+if [ $is_build_paddlepaddle -eq 1 ]; then
+     build_paddlepaddle
+fi
+
+
+if [ $is_build_capi_app_infer_image -eq 1 ]; then
+     build_capi_app_infer_image
+fi
+
+
+if [ $is_infer_capi -eq 1 ]; then
+     run_infer_capi
+fi
 
 
 #build_release_new
