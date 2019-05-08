19449a1b502d434cd82c309b2085a5a2fe1dc398
Pawel Piotrowicz <pablo.piotrowicz@gmail.com>
2019-05-08 12:51:17 +0200

    save xxxx

--- scripts/ngraph_builder.sh
+++ scripts/ngraph_builder.sh
@@ -50,9 +50,7 @@ source ref.txt
 function usage() {
 
      echo "Usage:  [---show-python-path]"
-     echo "Usage:  [--debug]  --cmake --build --run-infer-python" 
-     echo "Usage:  [--debug] --build-capi --run-infer-capi" 
-     echo "Usage:  [--debug] --build-paddlepaddle --build-capi-app-infer-image"
+     echo "Usage:  [--debug] --build-paddlepaddle --build-capi-app-infer-image --run-infer-capi --run-infer-python"
  
 }
 
@@ -194,6 +192,15 @@ function run_infer_capi()
         exit
     fi
 
+     if [ $is_debug -eq 0 ]; then
+          
+          export LD_LIBRARY_PATH="$build_dir_debug/python/paddle/libs" 
+          echo "LIBKI: $LD_LIBRARY_PATH"
+     fi
+     
+    export FLAGS_use_ngraph=true
+    export FLAGS_use_ngraph_cache=true
+      
      exe_cmd "cd $OPT_MODEL_CAPI_DIR_BUILD"
                                                                                        
      exe_cmd "numactl --membind=0 --physcpubind=0-19 ./infer_image_classification --infer_model="$SAVE_MODELS_DIR/$USE_MODEL" --data_list=$BIG_DATASET_LIST_FILE --data_dir=$BIG_DATASET_DIR_DATA --skip_batch_num=0 --batch_size=32 --iterations=100 --profile --paddle_num_threads=20 --use_mkldnn"
@@ -215,126 +222,22 @@ function build_paddlepaddle()
         exe_cmd "cd $build_dir_paddlepaddle"
 
 
-     TYPE_COMPILE="Release"
+    TYPE_COMPILE="Release"
 
     if [ $is_debug -eq 1 ]; then
        TYPE_COMPILE="Debug"
     fi
 
+    extra_options=""
 
-    exe_cmd "cmake .. -DCMAKE_BUILD_TYPE=$TYPE_COMPILE -DWITH_DOC=OFF -DWITH_GPU=OFF -DWITH_DISTRIBUTE=OFF -DWITH_MKLDNN=ON -DWITH_MKL=ON -DWITH_GOLANG=OFF -DWITH_STYLE_CHECK=OFF -DWITH_TESTING=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DWITH_PROFILER=OFF -DWITH_NGRAPH=ON -DWITH_FLUID_ONLY=ON"
+    extra_options="-DWITH_NGRAPH=ON -DWITH_FLUID_ONLY=ON"
 
-    exe_cmd "make -j $CORES"
-    exe_cmd "make -j $CORES  inference_lib_dist"
-
-
-
-}
-
-
-function build_release()
-{
-
-    echo "******* BUILD RELEASE *****"
-    
-   
-    if [ ! -d $build_dir_release ]; then
-         echo "Create directory $build_dir_release"
-         exe_cmd "mkdir $build_dir_release" 
-    fi
-
-    exe_cmd "cd $build_dir_release"
-
-    exe_cmd "cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_DOC=OFF -DWITH_GPU=OFF -DWITH_DISTRIBUTE=OFF -DWITH_MKLDNN=ON -DWITH_MKL=ON -DWITH_GOLANG=OFF -DWITH_STYLE_CHECK=OFF -DWITH_TESTING=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DWITH_PROFILER=OFF -DWITH_NGRAPH=ON -DWITH_FLUID_ONLY=ON"
+    exe_cmd "cmake .. -DCMAKE_BUILD_TYPE=$TYPE_COMPILE $extra_options -DWITH_DOC=OFF -DWITH_GPU=OFF -DWITH_DISTRIBUTE=OFF -DWITH_MKLDNN=ON -DWITH_MKL=ON -DWITH_GOLANG=OFF -DWITH_STYLE_CHECK=OFF -DWITH_TESTING=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DWITH_PROFILER=OFF "
 
     exe_cmd "make -j $CORES"
     exe_cmd "make -j $CORES  inference_lib_dist"
 
 
-}
-
-
-function build_release_cmake_new() {
-
-
- echo "******* BUILD RELEAS NEW  *****"
-
-
-    if [ ! -d $build_dir_release ]; then
-         echo "Create directory $build_dir_release"
-         exe_cmd "mkdir $build_dir_release"
-    fi
-
-    exe_cmd "cd $build_dir_release"
-
-    exe_cmd "cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_FLUID_ONLY=ON -DWITH_GPU=OFF -DWITH_MKLDNN=ON -DWITH_TESTING=ON -DWITH_PROFILER=ON -DWITH_STYLE_CHECK=OFF -DON_INFER=ON -DWITH_NGRAPH=ON"
-
-   # make -j50
-
-
-
-}
-
-
-
-function build_release_new() {
-
-
- echo "******* BUILD RELEAS NEW  *****"
-
-
-    if [ ! -d $build_dir_release ]; then
-         echo "Create directory $build_dir_release"
-         exe_cmd "mkdir $build_dir_release"
-    fi
-
-    exe_cmd "cd $build_dir_release"
-
-  #  cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_GPU=OFF -DWITH_MKLDNN=ON -DWITH_TESTING=ON -DWITH_PROFILER=ON -DWITH_STYLE_CHECK=OFF -DON_INFER=ON -DWITH_NGRAPH=ON
-
-    exe_cmd "make -j $CORES"
-    exe_cmd "make -j $CORES  inference_lib_dist"
-
-
-
-}
-
-
-function build_debug_cmake()
-{
-
-    echo "******* BUILD Debug *****"
-
-
-    if [ ! -d $build_dir_debug ]; then
-         echo "Create directory $build_dir_debug"
-         exe_cmd "mkdir $build_dir_debug"
-    fi
-
-
-    exe_cmd "cd $build_dir_debug"
-
-    exe_cmd "cmake .. -DCMAKE_BUILD_TYPE=Debug -DWITH_FLUID_ONLY=ON -DWITH_DOC=OFF -DWITH_GPU=OFF -DWITH_DISTRIBUTE=OFF -DWITH_MKLDNN=ON -DWITH_MKL=ON -DWITH_GOLANG=OFF -DWITH_STYLE_CHECK=OFF -DWITH_TESTING=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DWITH_PROFILER=OFF -DWITH_NGRAPH=ON"
-
-}
-
-
-
-function build_debug()
-{
-
-    echo "******* BUILD Debug *****"
-
-
-    if [ ! -d $build_dir_debug ]; then
-         echo "Create directory $build_dir_debug"
-        exe_cmd "mkdir $build_dir_debug"
-    fi
-
-    exe_cmd "cd $build_dir_debug"
-
-    exe_cmd "make -j $CORES" 
-    exe_cmd "make -j $CORES inference_lib_dist"
 
 }
 
@@ -385,6 +288,47 @@ function showpythonpath() {
 
 
 }
+
+
+
+function run_infer_image_python() {
+
+
+    echo "**** RUN INFER IMAGE *****"
+
+    if [ ! -f $IIC_SCRIPT ]; then
+        echo "FILE $IIC_SCIRPT not found"
+       exit 1
+    fi
+
+    prepare_model
+
+    if [ $is_debug -eq 1 ]; then 
+       export PYTHONPATH="$OPT_PADDLE_BUILD_DEBUG"
+    else
+       export PYTHONPATH="$OPT_PADDLE_BUILD_RELEASE"
+      
+       # fix for ../libs/libcpu_backend.so: undefined symbol: _ZN3tbb8internal17itt_task_begin_v7ENS0_15itt
+       export LD_LIBRARY_PATH="$build_dir_debug/python/paddle/libs"   
+       echo "LIBKI: $LD_LIBRARY_PATH"
+    fi
+    
+    echo "PYTHONPATH=$PYTHONPATH"
+
+    exe_cmd "cd $IIC_SCRIPT_DIR" 
+  #  python infer_image_classification.py --batch_size=1 --data_set=imagenet --iterations=400 --device=CPU --skip_batch_num=10 --infer_model_path=/data/PaddlePaddle/models/paddlepaddle/resnet_50_v1 --profile --use_fake_data
+
+    export FLAGS_use_ngraph=true
+    export FLAGS_use_ngraph_cache=true
+ 
+    ccmd="python infer_image_classification.py --batch_size=1 --data_set=imagenet --iterations=400 --device=CPU --skip_batch_num=10 --infer_model_path="$SAVE_MODELS_DIR/$USE_MODEL" --profile --use_fake_data"
+    $ccmd
+
+    
+}
+
+
+
 
 
 
@@ -459,24 +403,12 @@ do
    fi
 
 
-
    if [  "$item" = "--debug" ]; then
            echo "--debug found"
            is_debug=1
            was_used=1 
    fi
-
-   if [  "$item" = "--cmake" ]; then
-           echo "--cmake found"
-           is_cmake=1 
-           was_used=1
-   fi
-
-   if [  "$item" = "--build" ]; then
-           echo "--build found"
-           is_build=1 
-           was_used=1
-   fi
+  
 
    if [  "$item" = "--run-infer-python" ]; then
            echo "----run-infer-python found"
@@ -490,13 +422,7 @@ do
            is_infer_capi=1 
            was_used=1
    fi
-
-
-   if [  "$item" = "--build-capi" ]; then
-           echo "--build-capi found"
-           is_build_capi=1 
-           was_used=1
-   fi
+   
 
    if [ "$item" = "--build-capi-app-infer-image" ]; then
          is_build_capi_app_infer_image=1
@@ -536,55 +462,6 @@ else
 fi
 
 
-if [ $is_cmake -eq 1 ]; then
-
-    if [ $is_debug -eq 1 ]; then
-        echo "DEBUG IS SET !!"
-
-        
-        build_debug_cmake
-    else
-        
-        build_release_cmake_new
-
-    fi
-
-fi
-
-
-if [ $is_build -eq 1 ]; then
-
-     if [ $is_debug -eq 1 ]; then
-         echo "DEBUG IS SET !!"
-       build_debug
-     else
-       build_release_new
-     fi
-
-fi
-
-
-if [ $is_infer_python -eq 1 ]; then
-
-     if [ $is_debug -eq 1 ]; then
-         echo "DEBUG IS SET !!"
-        infer_image_debug
-     else
-        infer_image_release
-     fi
-
-fi
-
-
-if [ $is_build_capi -eq 1 ]; then
-
-      build_capi
-
-fi
-
-
-
-
 if [ $is_build_paddlepaddle -eq 1 ]; then
      build_paddlepaddle
 fi
@@ -600,18 +477,7 @@ if [ $is_infer_capi -eq 1 ]; then
 fi
 
 
-#build_release_new
-#build_release
-
-
-#build_debug
-#infer_image_debug
-
-
-
-#export LD_LIBRARY_PATH=   python/paddle/libs
-
-
-#build_release_new
-#infer_image_release
+if [ $is_infer_python -eq 1 ]; then
+        run_infer_image_python
+fi
 
